5f04733689164a4d491b23c40cffa35599279d76
Cecilia Imthurn <chispa.ceci@gmail.com>
2025-12-07 21:19:51 -0500

    Including plots

--- app.py
+++ app.py
@@ -912,6 +912,11 @@ The regression lines help confirm dominant linear tendencies.
 - **Winds vs SST** appear noisy, as winds are influenced by pressure gradients and ENSO, not just temperature.  
 """)
 
+    scatter_local_style("AT_21", "T_25")
+    scatter_local_style("RH_910", "T_25")
+    scatter_local_style("WU_422", "T_25")
+    scatter_local_style("WV_423", "T_25")
+
     # =====================================================
     # 🔸 ENSO-Colored Scatter Plots
     # =====================================================
